1f52e8b05a5ad66612a223a3666173957f2b6825
Ryota Aoki <aoki@thewoody.jp>
2014-09-15 18:44:32 +0900

    create variable of index-number

--- current/books/01/section01/js/01.js
+++ current/books/01/section01/js/01.js
@@ -4,4 +4,12 @@ function photoGallery() {
     return document.getElementById(id);
   }
 
+  // 現在どのエントリーを表示しているかを格納する
+  var crIndex = 0; // 値を初期化
+  var c = {
+    classL: "endL",
+    classR: "endR",
+    classOn: "on"
+  };
+
 }
